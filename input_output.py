c45d86e2b056a4b04ce181af5bd72c3449bfe8fc
mthom41 <mckayashe@gmail.com>
2022-01-13 14:01:43 -0700

    intro projects

---
+++ input_output.py
@@ -0,0 +1,4 @@
+print("Well hello there!")
+first_name = input("What's your first name?> ")
+last_name = input("Excellent! Now what is your last name?> ")
+print("Fantastic. Then I'll call you",last_name,"\n" + first_name, last_name)
