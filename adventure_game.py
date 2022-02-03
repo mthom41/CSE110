fd8aa5226a3b41b6ee2bd13e1d11ec5472b40b1a
mthom41 <mckayashe@gmail.com>
2022-02-03 14:58:01 -0700

    adv. game planning

---
+++ adventure_game.py
@@ -0,0 +1,36 @@
+'''Plan:
+You wake up in a cold stone cell. There are 2 doors, 1 is a staircase down and 1 is a hallway. both are lit
+DOWN or UP
+    DOWN
+    it gets colder and there aren't any lights. Do you RETURN or CONTINUE?
+        RETURN
+        you are now in a different room with a staircase leading up.
+        as you head up the the staircase the air grows warmer around you
+        at the top you meet a man who's entirely engulfed in flame. He asks why you're here, do you FIGHT or REASON
+            FIGHT
+            you died
+
+            REASON
+            the man is very understanding, helps warm your hand, and shows you the exit.
+
+        CONTINUE
+        you descend deeper and after a bend you see a light up ahead. 
+        upon reaching the light you find a room with food and a candle.
+        there is another path continuing through the room though. EXPLORE or STAY?
+            STAY
+            you are warm, you are happy, and you fall asleep to the delicious scent of bread baking never to wake again.
+
+            EXPLORE
+            you push bravely forwards into the next hallway. It twists and turns but you keep your hand on the wall so as to not get lost. 
+            hours later the ever-increasing cold and mind-bending passageways bring you to your knees, then to the ground. So cold...
+            so cold...
+    UP
+    you pass a few other empty rooms like yours, none with windows. Eventually you reach a hall in which there's a great feast being held. 
+    you are invited to stay and eat by many jolly strangers, begging you to enjoy the feast. do you STAY or LEAVE
+        STAY
+        there are only 3 foods in the entire banquet hall, even though hundreds are dining. Huge platters full of chicken and ornate goblets of wine, 
+        as well as periodic small bowls of carefully cleaned pomegranate. "The fruit is delicious! Come, eat!"
+        do you eat FRUIT or CHICKEN? 
+
+
+'''
